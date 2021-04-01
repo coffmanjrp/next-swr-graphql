d64b5833674553109511d50432d529a8edfdda63
coffmanjrp <coffmanjrp@gmail.com>
2021-03-31 19:43:18 -0700

    Create layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import '../styles/style.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Next SWR GraphQL</title>
+      </Head>
+      <header>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Queries</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/optimistic-ui">
+              <a>Optimistic UI</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/subscriptions">
+              <a>Subscriptions</a>
+            </Link>
+          </li>
+        </ul>
+      </header>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
